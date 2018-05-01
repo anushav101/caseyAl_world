3d5b6b54ff40eed482529e73df60388382cc8e4e
Anusha Venkatesan <anushav@andrew.cmu.edu>
2018-05-01 00:15:44 +0000

    Article Model 2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+    #Relationships
+    has_one :category
+    
+    #Validations
+    validates_presence_of :title
+    validates_presence_of :content
+
+    # Scopes
+    scope :alphabetical, -> { order('name') }
+    scope :active, -> { where(active: true) }
+    
+    
 end
